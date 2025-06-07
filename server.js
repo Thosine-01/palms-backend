4835a083bc82c1252f18e332448663e36919af6c
Thosine-01 <lawaltosine01@gmail.com>
2025-06-07 10:31:38 +0100

    deploying the palms backend

--- server.js
+++ server.js
@@ -37,6 +37,10 @@ app.post("/data/:id", async (req, res) => {
     }
 });
 
+app.get('/', (req, res) => {
+    res.send('Welcome to the Palms Backend API');
+});
+
 // Start server
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
